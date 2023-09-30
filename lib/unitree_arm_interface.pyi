0fbb9d67e4a3031adf508604a4b352a88b25820d
DavidGao <davidgao1013@gmail.com>
2023-09-30 09:09:21 -0700

    Expose more variables in lowstate and lowcmd

--- lib/unitree_arm_interface.pyi
+++ lib/unitree_arm_interface.pyi
@@ -358,7 +358,7 @@ class ArmInterface:
         self,
         q: npt.NDArray[np.float64],
         qd: npt.NDArray[np.float64],
-        tau: npt.NDArray[np.float64] = npt.NDArray[np.float64](),
+        tau: npt.NDArray[np.float64] = np.zeros(0, dtype=np.float64),
     ) -> None:
         """
         Function: Set six joints commands to class lowcmd
