0f31359ad668fcd0bb94f8d77bb65f3db4edb26e
Yihuai Gao <yihuai@stanford.edu>
2023-10-15 03:40:10 +0000

    Update pid controller

--- examples_py/pid_test.py
+++ examples_py/pid_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import unitree_arm_interface as sdk
 import time
 import numpy.typing as npt
+from typing import Optional
 
 class Controller:
     """Base PID controller"""
@@ -22,32 +23,44 @@ class Controller:
         self.prev_error = np.zeros(self.input_size, dtype=np.float64)
         self.integral = np.zeros(self.input_size, dtype=np.float64)
 
-    def update(self, setpoint: npt.NDArray[np.float64], measured_value: npt.NDArray[np.float64]):
-        """Output velocity will be in rad/s"""
+    def update(self, setpoint: npt.NDArray[np.float64], measured_value: npt.NDArray[np.float64], debug_joint:Optional[int]=None):
+        """Output velocity will be in rad/s
+        Note that the joint number starts from 1 and is different from the numpy array index. 
+        See https://dev-z1.unitree.com/brief/parameter.html for detailed information."""
         error = setpoint - measured_value
         self.integral += error 
         derivative = error - self.prev_error
         output = self.kp * error + self.ki * self.integral + self.kd * derivative
         self.prev_error = error
         output = output.clip(-self.output_limit, self.output_limit)
+        if debug_joint:
+            print(f"Joint{debug_joint}: Target {setpoint[debug_joint-1]:+.03f} \
+Current {measured_value[debug_joint-1]:+.03f} \
+Error {error[debug_joint-1]:+.03f} \
+P {self.kp * error[debug_joint-1]:+.03f} \
+I {self.ki * self.integral[debug_joint-1]:+.03f} \
+D {self.kd * derivative[debug_joint-1]:+.03f} \
+Output {output[debug_joint-1]:+.03f} \
+")
         return output
 
 def pidAdjust(arm: sdk.ArmInterface, target_joint: npt.NDArray[np.float64], max_time: float):
 
-    dt = arm._ctrlComp.dt
+    dt = arm._ctrlComp.dt*10
     steps = max_time/dt
     arm.startTrack(sdk.ArmFSMState.JOINTCTRL)
     output_limit = 0.5
-    controller = Controller(1, 0.01, 0.01, 7, output_limit)
+    controller = Controller(3, 0.0005, 1, 7, output_limit)
     output_velocity = controller.update(target_joint, np.array(arm.lowstate.q))
 
     for i in range(int(steps)):
-        output_velocity = controller.update(target_joint, np.array(arm.lowstate.q))
+        output_velocity = controller.update(target_joint, np.array(arm.lowstate.q), debug_joint=1)
         assert np.all(np.abs(output_velocity) <= output_limit)
-        arm.jointCtrlCmd(output_velocity, 1)
-        if i % 50 == 0:
-            print(", ".join(f"{q:+.03f} ({v:+.03f})" for (q, v) in zip(arm.lowstate.q, output_velocity)))
-        time.sleep(dt)
+        # if i % 50 == 0:
+            # print(", ".join(f"{q:+.03f} ({v:+.03f})" for (q, v) in zip(arm.lowstate.q, output_velocity)))
+        for j in range(int(dt/arm._ctrlComp.dt)):
+            arm.jointCtrlCmd(output_velocity, 1)
+            time.sleep(arm._ctrlComp.dt)
 
 
 
@@ -57,5 +70,13 @@ arm =  sdk.ArmInterface(hasGripper=True)
 
 targetPos = np.zeros(7, dtype=np.float64)
 targetPos[0] = 0.5
+targetPos[2] = -0.3
 arm.loopOn()
-pidAdjust(arm, targetPos, 10)
+
+arm.backToStart()
+
+pidAdjust(arm, targetPos, max_time=10)
+
+time.sleep(1)
+arm.backToStart()
+arm.loopOff()
